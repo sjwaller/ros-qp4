3c80bcffceb69f6fae96600599b5beafffad3d3e
Stewart Waller <sjwaller@gmail.com>
2016-11-18 00:19:25 +0000

    removed dev folders

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stewart/Robotics/ros-qp4/src
-# Build directory: /home/stewart/Robotics/ros-qp4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(qp4_master)
